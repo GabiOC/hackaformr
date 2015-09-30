e16021052e4f6f741da9ee66274026dce420301e
staceyko <ko.stacey@gmail.com>
2015-09-30 14:47:49 -0400

    add team controller/ button

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,12 @@
-# class RegistrationsController < Devise::RegistrationsController
-#   protected
-#
-#   # def after_sign_up_path_for(resource)
-#   #   # @current_user = User.find(session[:user_id])
-#   #   if current_user.organizer
-#   #   redirect_to '/users/organizer_show' # Or :prefix_to_your_route
-#   #   else
-#   #   redirect_to '/users/user_show'
-#   #   end
-#   # end
-# end
+class RegistrationsController < Devise::RegistrationsController
+  protected
+
+  # def after_sign_up_path_for(resource)
+  #   # @current_user = User.find(session[:user_id])
+  #   if current_user.organizer
+  #   redirect_to '/users/organizer_show' # Or :prefix_to_your_route
+  #   else
+  #   redirect_to '/users/user_show'
+  #   end
+  # end
+end
