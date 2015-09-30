fed549f6c227b6776421b4b80ced0db249b8ae9d
staceyko <ko.stacey@gmail.com>
2015-09-30 15:00:21 -0400

    teams controller

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,15 @@
+class TeamsController < ApplicationController
+
+  def show
+  end
+
+  def create
+    @hackathon = Hackathon.find_by_id(params["hackathon_id"])
+  
+
+  end
+
+  def new
+  end
+
+end
