884d6ecd0bcefd21d4405e4fc7813e9986a6f6f7
emottk <emottk@gmail.com>
2015-09-30 11:20:59 -0400

    migrations again

--- db/migrate/20150930145354_drop_user_skills.rb
+++
@@ -1,5 +0,0 @@
-class DropUserSkills < ActiveRecord::Migration
-  def change
-    drop_table :user_skills
-  end
-end
--- db/migrate/20150930145447_drop_user_teams.rb
+++
@@ -1,5 +0,0 @@
-class DropUserTeams < ActiveRecord::Migration
-  def change
-    drop_table :user_teams
-  end
-end
