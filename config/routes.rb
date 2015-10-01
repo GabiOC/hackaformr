e19b2c3b4218369afba11416cb880142129c0c9b
staceyko <ko.stacey@gmail.com>
2015-10-01 16:02:57 -0400

    Add team show page and hackathon new page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
   resources :users, :hackathons, :participants, :teams
 
+  post '/hackathons/create', to: 'hackathons#create'
   post '/participants/create', to: 'participants#create'
   post '/teams/create', to: 'teams#create'
 end
