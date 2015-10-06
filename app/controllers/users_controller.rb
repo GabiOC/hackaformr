a3c9097df116ab86821c8547d4f79884cffbaab5
staceyko <ko.stacey@gmail.com>
2015-10-05 22:45:46 -0400

    add UserNotifier method to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,18 @@ class UsersController < ApplicationController
 
   def new
   end
+  
+  def create
+      # Create the user from params
+      @user = User.new(params[:user])
+      if @user.save
+        # Deliver the signup email
+        UserNotifier.send_signup_email(@user).deliver
+        redirect_to(@user, :notice => 'User created')
+      else
+        render :action => 'new'
+      end
+    end
 
   def add_hackathon
     @hackathon = Hackathon.find_by_id(params["hackathon_id"])
