03a2b7a18b26cb37c193c65f66294cb45edb2915
GabiOC <oconnor.gabriele@gmail.com>
2015-10-09 12:54:18 -0400

    Convert post to get in hackathon_teams_path

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -6,11 +6,10 @@ class TeamsController < ApplicationController
   def create
     @hackathon = Hackathon.find_by_id(params["hackathon_id"])
     @teams = Team.for(@hackathon)
+    # remove previous teams in case teams generated multiple times
+    @hackathon.teams = []
     @teams.each do |user_array|
       team_object = Team.create
-      # remove previous teams in case teams generated multiple times
-      @hackathon.teams = []
-      # add teams to hackathon
       @hackathon.teams << team_object
       user_array.each do |u|
         u.teams << team_object
@@ -19,5 +18,8 @@ class TeamsController < ApplicationController
     end
     session[:hackathon_id] = @hackathon.id
     redirect_to hackathon_teams_path(@hackathon.id)
+  end
+
+  def index
   end
 end
